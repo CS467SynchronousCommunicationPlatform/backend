3320dc9fae07ccbf71eab912c02108b4f9d7d4f2
Blake Hakkila <hakkilab@oregonstate.edu>
2024-10-24 05:47:32 -0700

    basic web socket server messaging example with rabbitmq

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 3000
+/*
+RabbitMQ (AMQP) code based on promise api docs (https://amqp-node.github.io/amqplib/channel_api.html) and 
+RabbitMQ Javascript Hello World example (https://www.rabbitmq.com/tutorials/tutorial-one-javascript)
+Websocket code based on ws api docs (https://github.com/websockets/ws/blob/master/doc/ws.md)
+*/
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const websocket = require("ws")
+const amqp = require('amqplib');
+require('dotenv').config()
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const wss = new websocket.Server({ port: 8000 })
+const GENERAL = "general";
+
+let clients = new Map()
+let channel;
+
+amqp.connect(process.env.RABBITMQ_URL)
+  .then(connection => connection.createChannel())
+  .then(chan => {
+    chan.assertQueue(GENERAL, { durable: true });
+    channel = chan;
+    chan.consume(GENERAL, message => {
+      for (const sock of wss.clients) {
+        sock.send(message.content.toString())
+      }
+    })
+  }).catch(err => {
+    throw err;
+  });
+
+wss.on("connection", ws => {
+  ws.on("message", message => {
+    let json = JSON.parse(message)
+    for (const key of ["id", "type", "message"]) {
+      if (json[key] === undefined) {
+        ws.send(JSON.stringify({ "error": `${key} was not provided in message` }));
+        return;
+      }
+    }
+    clients[json.id] = ws;
+    channel.sendToQueue(json.type, Buffer.from(json.message));
+  })
+});
