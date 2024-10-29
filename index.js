46969096f133ead85b05c6ebb1192867b9cae7b6
Blake Hakkila <hakkilab@oregonstate.edu>
2024-10-29 01:56:52 -0700

    refactored backend to remove rabbitmq and shift to socket.io with express, updated package.json

--- index.js
+++ index.js
@@ -1,59 +1,39 @@
 /*
-RabbitMQ (AMQP) code based on promise api docs (https://amqp-node.github.io/amqplib/channel_api.html) and 
-RabbitMQ Javascript Hello World example (https://www.rabbitmq.com/tutorials/tutorial-one-javascript)
-Websocket code based on ws api docs (https://github.com/websockets/ws/blob/master/doc/ws.md)
+Basic server structure integrating socket.io and express taken from Socket.io tutorial
+https://socket.io/docs/v4/tutorial/step-3
 */
+import dotenv from "dotenv";
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+dotenv.config();
 
-const websocket = require("ws")
-const amqp = require('amqplib');
-require('dotenv').config()
+// server constants
+const app = express();
+const server = createServer(app);
+const io = new Server(server, { transports: ["websocket"] });
 
-const wss = new websocket.Server({ port: 8000 })
-const GENERAL = "general";
+// chat constants and globals
+let clients = new Map();
 
-let clients = new Map()
-let channel;
+io.on("connection", (socket) => {
+  // add socket to map of client connections
+  clients.set(socket.id, socket);
+  console.log(`${socket.id} client connected`);
 
-// connect to hosted rabbitmq
-amqp.connect(process.env.RABBITMQ_URL)
-  .then(connection => connection.createChannel())
-  .then(chan => {
-    // create general chat channel
-    chan.assertQueue(GENERAL, { durable: true });
-    channel = chan;
-
-    // register consumer for general chat, sends message to all sockets
-    chan.consume(GENERAL, message => {
-      for (const sock of wss.clients) {
-        sock.send(message.content.toString())
-      }
-    })
-  }).catch(err => {
-    throw err;
+  // register disconnect to remove socket from client connections
+  socket.on("disconnect", () => {
+    clients.delete(socket.id);
+    console.log(`${socket.id} client disconnected`);
   });
+});
 
-// handle websocket connections
-wss.on("connection", ws => {
-  ws.on("message", message => {
-    // verify structure of message from client
-    let json;
-    try {
-      json = JSON.parse(message)
-    } catch {
-      ws.send(JSON.stringify({ "error": `invalid json` }));
-      return;
-    }
-    for (const key of ["id", "type", "message"]) {
-      if (json[key] === undefined) {
-        ws.send(JSON.stringify({ "error": `${key} was not provided in json` }));
-        return;
-      }
-    }
+// basic REST endpoint
+app.get("/", (req, res) => {
+  res.send("Backend REST API running")
+})
 
-    // map from client id to websocket connection
-    clients[json.id] = ws;
-
-    // forward message to rabbitmq
-    channel.sendToQueue(json.type, Buffer.from(json.message));
-  })
+// start server
+server.listen(process.env.PORT, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT}`);
 });
