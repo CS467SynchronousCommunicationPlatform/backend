e21d39a0292ec05458786efa21fd5023eeff9d29
evong <evong1992@gmail.com>
2024-10-31 01:01:53 -0700

    add users in channel endpoint

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ import dotenv from "dotenv";
 import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+import { createClient } from "@supabase/supabase-js"
 dotenv.config();
 
 // server constants
 const app = express();
 const server = createServer(app);
 const io = new Server(server, { transports: ["websocket"] });
+
+// supabase client
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
 
 // chat constants and globals
 let clients = new Map();
@@ -31,6 +35,16 @@ io.on("connection", (socket) => {
 // basic REST endpoint
 app.get("/", (req, res) => {
   res.send("Backend REST API running")
+})
+
+app.get("/users/:channelId", async (req, res) => {
+  let channelId = req.params.channelId;
+  const { data, error } = await supabase
+    .from('users')
+    .select('display_name, channels_users!inner()')
+    .eq('channels_users.channel_id', channelId)
+    
+  res.send(data)
 })
 
 // start server
