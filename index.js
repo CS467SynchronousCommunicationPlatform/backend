6c6984b40415cbe1f714a7acef9594e1b839bee8
Blake Hakkila <hakkilab@oregonstate.edu>
2024-10-31 20:49:16 -0700

    added general chat message socket listener and message validation

--- index.js
+++ index.js
@@ -22,6 +22,29 @@ function error(socket, message) {
   console.error(message);
 }
 
+// helper function to validate general chat message
+function isValidGeneralMessage(socket, message) {
+  // check that the message has all required fields and they are strings
+  for (const field of ["body", "user_id", "timestamp"]) {
+    if (message[field] === undefined) {
+      error(socket, `General message missing "${field}" property`);
+      return false;
+    }
+    if (typeof message[field] !== "string") {
+      error(socket, `General message "${field}" property is not a string`)
+      return false;
+    }
+  }
+
+  // check if created_at is a valid timestamp
+  if (new Date(message.timestamp).toString() === "Invalid Date") {
+    error(socket, `General message "timestamp" is "${message.created_at}" which is not a valid timestamp`)
+    return false;
+  }
+
+  return true;
+}
+
 // Registers a client socket connection with the backend
 function registerConnection(socket) {
   // verify the connection has a token
@@ -45,10 +68,28 @@ function registerConnection(socket) {
   return true;
 }
 
+// Registers a client socket connection with the backend
+function registerGeneralChatListener(socket) {
+  socket.on("general", (message) => {
+    // validate message contents
+    if (!isValidGeneralMessage(socket, message))
+      return;
+
+    // send general chat message to every other user
+    for (const [user, other] of clients.entries()) {
+      if (user !== socket.handshake.auth.token)
+        other.emit("general", message);
+    }
+  });
+}
+
 io.on("connection", (socket) => {
   // handle registering the client connection, return on failure
   if (!registerConnection(socket))
     return;
+
+  // register listeners for general chat
+  registerGeneralChatListener(socket);
 });
 
 // basic REST endpoint
