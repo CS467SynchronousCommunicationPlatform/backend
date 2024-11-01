e2948ee1260239cad70a23f08a53cc641c73c817
Blake Hakkila <hakkilab@oregonstate.edu>
2024-10-31 20:50:41 -0700

    Added test script that shows how to connect client

---
+++ test.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import { io } from "socket.io-client";
+dotenv.config();
+
+// connect a few clients to the server
+const SERVER = `http://localhost:${process.env.PORT}`
+const socket1 = io(SERVER, { auth: { token: process.env.TEST_USER1 }, transports: ["websocket"] })
+const socket2 = io(SERVER, { auth: { token: process.env.TEST_USER2 }, transports: ["websocket"] })
+
+// listen for general chat messages
+socket1.on("general", (msg) => console.log("user1 received:", msg));
+socket2.on("general", (msg) => console.log("user2 received:", msg));
+
+// send a general chat message
+socket1.emit("general", { body: "Hello", user_id: process.env.TEST_USER1, timestamp: new Date().toISOString() });
+socket2.emit("general", { body: "World", user_id: process.env.TEST_USER2, timestamp: new Date().toISOString() });
