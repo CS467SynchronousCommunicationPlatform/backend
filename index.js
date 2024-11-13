fe9c0744372b89bbaa0954c1262b183ef8e4e6cf
evong <evong1992@gmail.com>
2024-11-13 00:47:39 -0800

    refactor increase and decrease notification function into one

--- index.js
+++ index.js
@@ -197,6 +197,16 @@ app.get("/channels/:channelId/messages", async (req, res, next) => {
   }
 });
 
+// endpoint for updating unread notifications
+app.post("/notifications/channels/users", async (req, res, next) => {
+  try {
+    const { data, error, status } = await model.updateUnreadMessage(req.body.function, req.body.userId, req.body.channelId);
+    console.log(error)
+  } catch (err) {
+    next(err)
+  }
+});
+
 // initializes backend with necessary data from database
 async function initializeBackend() {
   // get all display names for registered users
--- model.js
+++ model.js
@@ -39,14 +39,9 @@ export async function readAllChannelsForUser(userId) {
     .eq('channels_users.user_id', userId);
 }
 
-export async function incUnreadMessage(userId, channelId) {
+export async function updateUnreadMessage(func, userId, channelId) {
   return await supabase
-    .rpc('increment', {x:1, user_id: userId, channel_id: channelId})
-}
-
-export async function decUnreadMessage(userId, channelId) {
-  return await supabase
-    .rpc('decrement', {x:1, user_id: userId, channel_id: channelId})
+    .rpc(func, {userid: userId, channelid: channelId});
 }
 
 // messages functions
